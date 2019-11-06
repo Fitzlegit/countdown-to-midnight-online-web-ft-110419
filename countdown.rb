61adc8488f84ea177e10ba0989d9e450a6f75fd5
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-06 13:20:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,19 +3,17 @@ require 'pry'
 #write your code here
 def countdown_with_sleep(num)
   while num > 0
-  puts "#{num} SECOND(S)!"
-  sleep(num)
-  num -= 1
-end
-  
+    puts "#{num} SECOND(S)!"
+    sleep(num)
+    num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
 
 
 def countdown(num)
-  seconds_num = 1
-  while num >= 0
+  while num > 0
     puts "#{num} SECOND(S)!"
-    countdown_with_sleep(seconds_num)
     num -= 1
   end
   p "HAPPY NEW YEAR!"
