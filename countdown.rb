fa5beec514ff7dbcff8fde68f9ccf348f273ab79
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-06 13:10:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,13 +5,16 @@ def countdown_with_sleep(num)
   sleep(num)
 end
 
+def countdown_numbers(num)
+  while num == 0
+   puts "#{num} SECOND(S)!"
+   num -= 1
+  end
+end
 
-def countdown(num)
+def countdown
   seconds_num = 1
-  while num >= 0
-    puts "#{num} SECOND(S)!"
     countdown_with_sleep(seconds_num)
     num -= 1
-  end
   p "HAPPY NEW YEAR!"
 end
