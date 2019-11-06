ce52cc5bd38795ebbc8d37cc839727b654c27e2d
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-06 12:44:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,9 @@ require 'pry'
 #write your code here
 
 def countdown(num)
-  num = 10
-  while num == 0
-     "#{num} SECOND(S)!"
+  number = 10
+  while number == 0
+     "#{number} SECOND(S)!"
      num -= 1
   end
   p "HAPPY NEW YEAR!"
